b9be1ba85b0e6c7e99e8a19e92cbd1954d61c86c
Santiago Parra <143861114+AsTEKA01@users.noreply.github.com>
2024-08-06 20:18:31 -0500

    Update users.sql

--- Database/Mysql/users.sql
+++ Database/Mysql/users.sql
@@ -19,12 +19,10 @@ CREATE TABLE users (
 
 -- INSERCCION DE LOS DATOS DENTRO DE LA TABLA DE "USERS" (Usuarios/Empleados) --
 
-INSERT INTO id_users,name,edad,genero,email,password (
+INSERT INTO users (id_users, name,edad,genero,email,password) 
     VALUES 
     (1, "Santiago Camacho Parra", 18,"Masculino", "snatiago128@gmail.com","admin123"),
     (2, "Laura Vanesa Gutierrez Lozano", 18,"Femenino", "lauragutierrez01@gmail.com","empanada1212"),
     (3, "Juan Sebastian Tovar Roa", 19,"Masculino", "tovarroa09@gmail.com","root9090"),
     (4, "Stiven Ramirez Lugo", 21,"Masculino", "ramirezlugo50@gmail.com","ramirezlugo65"),
-    (5, "Maria Victoria Chavarro", 23,"Femenino", "mariavictoria001@gmail.com","1234567")
-
-);
+    (5, "Maria Victoria Chavarro", 23,"Femenino", "mariavictoria001@gmail.com","1234567");
